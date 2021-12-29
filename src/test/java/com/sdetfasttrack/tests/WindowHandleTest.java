d7dad91ec26e5651198353eb31b1f8aed2d60655
daule <daulet2020@gmail.com>
2021-12-28 19:07:39 -0600

    First

--- src/test/java/com/sdetfasttrack/tests/WindowHandleTest.java
+++ src/test/java/com/sdetfasttrack/tests/WindowHandleTest.java
@@ -1,4 +1,53 @@
 package com.sdetfasttrack.tests;
 
+import com.sdetfasttrack.pages.WindowHandlePage;
+import com.sdetfasttrack.utilities.Driver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Set;
+
 public class WindowHandleTest {
+
+    WindowHandlePage windowHandlePage = new WindowHandlePage();
+
+    @Test
+    public void windowHandleTest() {
+
+
+        //TC#4
+        //1. Go to http://practice.cydeo.com/windows
+        Driver.getDriver().get("http://practice.cydeo.com/windows");
+
+        //2. Assert: Title is "Practice"
+        String expTitle = "Practice";
+        String actTitle = Driver.getDriver().getTitle();
+        Assert.assertEquals(expTitle, actTitle, "Title did not match");
+
+        //3. Click to: "Click Here" text
+        windowHandlePage.clickHereBtn.click();
+
+        String mainHandle = Driver.getDriver().getWindowHandle();
+        //System.out.println("main"+mainHandle);
+
+        Set<String> windHandles = Driver.getDriver().getWindowHandles();
+
+        //4. Switch to new Window
+        for (String eachTab:windHandles){
+            Driver.getDriver().switchTo().window(eachTab);
+            String title = Driver.getDriver().getTitle();
+            System.out.println(eachTab);
+            if (title.contains("New Window")){
+                break;
+            }
+        }
+        //5 Assert: Title is "New Window"
+        expTitle = "New Window";
+        actTitle=Driver.getDriver().getTitle();
+        Assert.assertEquals(actTitle,expTitle,"Title not match");
+
+        //6 Switch back to main handle
+        Driver.getDriver().switchTo().window(mainHandle);
+
+    }
 }
