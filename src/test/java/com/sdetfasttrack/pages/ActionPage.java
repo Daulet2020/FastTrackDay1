9e746afa5d2360f2c60be1cba1d9dd0822527d5b
daule <daulet2020@gmail.com>
2021-12-29 17:37:43 -0600

    First

---
+++ src/test/java/com/sdetfasttrack/pages/ActionPage.java
@@ -0,0 +1,52 @@
+package com.sdetfasttrack.pages;
+
+import com.sdetfasttrack.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ActionPage {
+
+
+    //TC#1
+    //1. Go to http://practice.cydeo.com/hovers
+    //2. Hover over to first image
+    //3. Assert: “name: user1” is displayed
+    //4. Hover over to second image
+    //5. Assert: “name: user2” is displayed
+    //6. Hover over to third image
+    //7. Assert: “name: user3” is displayed
+
+
+    //TC#2
+    //1. Go	to https://the-internet.herokuapp.com/context_menu
+    //2. Right click to the	box.
+    //3. Alert will open.
+    //4. Accept	alert.
+
+    public ActionPage() { //constructor
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//*[@class='figure'][1]")
+    public WebElement image1;
+
+    @FindBy(xpath = "//*[@class='figure'][2]")
+    public WebElement image2;
+
+    @FindBy(xpath = "//*[@class='figure'][3]")
+    public WebElement image3;
+
+    @FindBy(xpath = "//*[.='name: user1']")
+    public WebElement user1;
+
+    @FindBy(xpath = "//*[.='name: user2']")
+    public WebElement user2;
+
+    @FindBy(xpath = "//*[.='name: user3']")
+    public WebElement user3;
+
+    }
+
+
+
---
+++ src/test/java/com/sdetfasttrack/tests/ActionClassTest.java
@@ -0,0 +1,48 @@
+package com.sdetfasttrack.tests;
+
+import com.sdetfasttrack.pages.ActionPage;
+import com.sdetfasttrack.utilities.Driver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import javax.swing.*;
+
+public class ActionClassTest {
+
+    ActionPage actionPage = new ActionPage(); // object of ActionPage
+
+    @Test
+    public void actionTest1() {
+        //TC#1
+        //1. Go to http://practice.cydeo.com/hovers
+        Driver.getDriver().get("http://practice.cydeo.com/hovers");
+
+        Actions actions = new Actions(Driver.getDriver()); //creating object of Actions class
+
+        //2. Hover over to first image
+        actions.moveToElement(actionPage.image1).build().perform();
+
+        //3. Assert: “name: user1” is displayed
+        Assert.assertTrue(actionPage.user1.isDisplayed(), "user1 not displayed");
+
+        //4. Hover over to second image
+        actions.moveToElement(actionPage.image2).build().perform();
+
+        //5. Assert: “name: user2” is displayed
+        Assert.assertTrue(actionPage.image2.isDisplayed(), "user2 not displayed");
+
+        //6. Hover over to third image
+        actions.moveToElement(actionPage.image3).build().perform();
+
+        //7. Assert: “name: user3” is displayed
+        Assert.assertTrue(actionPage.image3.isDisplayed(), "user3 not displayed");
+
+        Driver.closeDriver();
+
+
+
+
+    }
+
+}
